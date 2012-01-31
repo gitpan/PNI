ed61bcf32c0babac5fae38d6c29d8d5d0a33ad4c
Gianluca Casati <fibo@cpan.org>
2012-01-31 21:22:31 +0000

    Import of FIBO/PNI-0.26 from CPAN.
    gitpan-cpan-distribution: PNI
    gitpan-cpan-version:      0.26
    gitpan-cpan-path:         FIBO/PNI-0.26.tar.gz
    gitpan-cpan-author:       FIBO
    gitpan-cpan-maturity:     released

--- lib/PNI.pm
+++ lib/PNI.pm
@@ -3,7 +3,12 @@ package PNI;
 use strict;
 use warnings;
 
-our $VERSION = '0.25';
+# As recommended in permodstyle's "Version numbering" section
+# considering version x.yy stable, a new release will be versioned
+# as x.yy_zz so it will not be listed by CPAN.pm as the last.
+# After test results will be ok, it can be officially released x.(yy+1) version.
+our $VERSION = '0.26';
+$VERSION = eval $VERSION;
 
 use Exporter 'import';
 use PNI::Edge;
